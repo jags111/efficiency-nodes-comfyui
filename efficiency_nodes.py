fe6b232db357319b09cef85d90d44bf1f964028b
curious.jp <48515264+curiousjp@users.noreply.github.com>
2024-10-07 07:30:07 +1100

    only cleanup where prompt graph exists

--- efficiency_nodes.py
+++ efficiency_nodes.py
@@ -134,7 +134,8 @@ class TSC_EfficientLoader:
                         ascore=None, prompt=None, my_unique_id=None, loader_type="regular"):
 
         # Clean globally stored objects
-        globals_cleanup(prompt)
+        if prompt:
+            globals_cleanup(prompt)
 
         # Create Empty Latent
         latent = torch.zeros([batch_size, 4, empty_latent_height // 8, empty_latent_width // 8]).cpu()
@@ -722,7 +723,8 @@ class TSC_KSampler:
 
         # ---------------------------------------------------------------------------------------------------------------
         # Clean globally stored objects of non-existant nodes
-        globals_cleanup(prompt)
+        if prompt:
+            globals_cleanup(prompt)
 
         # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
         # If not XY Plotting
